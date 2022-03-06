1a071b0ea552f3b7528929d3146c54d1dd0ced47
Aramais Garibjan <aramo.g@hotmail.de>
2022-03-06 23:53:25 +0100

    fix 'No results found' if statement

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -65,7 +65,7 @@ export function ItemList() {
         onChange={(e) => onSearchbarChange(e.target.value)}
         placeholder="Enter your search"
       ></input>
-      {searchList.length === 0 && <p>No results found</p>}
+      {activeList.length === 0 && <p>No results found</p>}
       <ul>
         {searchList.map((item) => (
           <SearchListButton
