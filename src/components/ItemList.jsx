ff7c190d87adcb12c7851b54e841b5f1ec5b643d
Niklas Röttger <niklas.roettger@outlook.de>
2022-03-08 14:24:44 +0100

    add localstorage

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -5,10 +5,22 @@ import { ActiveListButton } from "./ActiveListButton";
 
 const ApiURL = "https://fetch-me.vercel.app/api/shopping/items";
 
+function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+function setLocalStorage(key, value) {
+  return localStorage.setItem(key, JSON.stringify(value));
+}
+
 export function ItemList() {
-  const [shoppingList, setShoppingList] = useState([]);
+  const [shoppingList, setShoppingList] = useState(
+    () => getLocalStorage("shoppingList") ?? []
+  );
   const [searchList, setSearchList] = useState([]);
-  const [activeList, setActiveList] = useState([]);
+  const [activeList, setActiveList] = useState(
+    () => getLocalStorage("activeList") ?? []
+  );
 
   useEffect(() => {
     FetchData();
@@ -16,12 +28,19 @@ export function ItemList() {
       try {
         const response = await fetch(ApiURL);
         const data = await response.json();
-        setShoppingList(data.data);
+        if (shoppingList.length === 0) {
+          setShoppingList(data.data);
+        }
       } catch (error) {
         console.log(error);
       }
     }
   }, []);
+
+  useEffect(() => {
+    setLocalStorage("activeList", activeList);
+    setLocalStorage("shoppingList", shoppingList);
+  }, [activeList]);
 
   function onButtonClick(buttonId) {
     setActiveList([
@@ -35,12 +54,20 @@ export function ItemList() {
 
   function onSearchbarChange(searchInput) {
     setSearchList(
-      search(searchInput, shoppingList, {
-        keySelector: (item) => item.name.de,
-      })
+      search(
+        searchInput,
+        shoppingList.filter(
+          (item) =>
+            !activeList.some((activeItem) => {
+              activeItem._id === item._id;
+            })
+        ),
+        {
+          keySelector: (item) => item.name.de,
+        }
+      )
     );
   }
-
   function onActiveButtonClick(buttonId) {
     setActiveList(activeList.filter((item) => item._id !== buttonId));
     setShoppingList([
